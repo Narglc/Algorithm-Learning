949b264c343600a37103d89c802adc54b03df8b2
Narglc <qingcheliuzhi@qq.com>
2018-04-23 22:37:48 +0800

    KMP algorithm

---
+++ KMP/ViolentMath.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<string.h>
+
+//暴力匹配算法
+int violentMatch(char* S,char* P){
+	int lenS=strlen(S);
+	int lenP=strlen(P);
+
+	int i,j=0;
+	for(i=0;i<(lenS-lenP+1);i++){
+		for(j=0;j<lenP;j++){
+			if(S[i+j]!=P[j]){
+				break;
+			}else if(j==lenP-1){
+				return i;
+			}
+		}
+	}
+	return -1;
+		
+
+/*
+	if(lenS>lenP){
+		return lenS;
+	}else{
+		return lenP;
+	}
+*/
+}
+
+int betterViolentMatch(char* S,char* P){
+	int lenS=strlen(S);
+	int lenP=strlen(P);
+
+	int i,j=0;
+	while(i<lenS && j<lenP){
+		//1. 如果当前字符串匹配成功，则i++，j++
+		if(S[i]==P[j]){
+			i++;j++;
+		}else{
+		//2. 如果失配，令i=i-(j-1),j=0
+			i=i-j+1;j=0;
+		}
+	}
+
+	//匹配成功，返回模式串P在S的位置，否则返回-1
+	if(j==lenP){
+		return i-j;
+	}else{
+		return -1;
+	}
+}
+
+
+
+main(){
+	//字符串赋值用法及实际长度
+	char sr[78]="f3irjigj";
+	printf("长度为%d\n",strlen(sr));
+	
+
+	//测试案例
+	char* stringS="BBC ABCDAB ABCDABCDABDE";
+	char* stringP="ABCDABD";
+
+	int index=0;
+	index=violentMatch(stringS,stringP);
+	
+	if(index==-1){
+		printf("在字符串S中未找到字符串P！\n");
+	}else{
+		printf("结果为:%d\n",index);
+	}
+	
+	int betterIndex=betterViolentMatch(stringS,stringP);
+	printf("better result:%d\n",betterIndex);
+}
--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # Algorithm-Learning
 
+## 计算函数运行所用时间
+???
+```
+#include<time.h>
+
+clock_t begin,end;
+double cost;
+begin=clock();
+func();
+end=clock();
+cost=(double)(end-begin)/CLOCK_PER_SECS;
+printf("耗时:%lf secs",cost);
+```
+
+
 ## KMP算法 (2018-4-23)
+Knuth-Morris-Pratt字符串查找算法，简称“KMP算法”。常用于在一个文本串S内查找一个模式串P 的出现位置，这个算法由Donald Knuth、Vaughan Pratt、James H. Morris三人于1977年联合发表，故取这3人的姓氏命名此算法。
+
+**算法流程：**
+
 
